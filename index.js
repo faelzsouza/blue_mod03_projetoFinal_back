36c50950b764d208ba2453cba7c2b1e072749e29
Rafael Souza <faelzsouza@gmail.com>
2021-09-15 10:43:09 -0300

    cors able

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 require("dotenv").config();
 const express = require("express");
 const db = require("./models/database/db");
+const cors = require('cors')
 
 const app = express();
 const port = 3000;
+const corsOptions = {
+    origin: 'http://localhost:3000',
+    optionsSuccessStatus: 200
+}
 
 const dbUrl = process.env.DB_URL;
 const dbUser = process.env.DB_USER;
@@ -12,6 +17,7 @@ const dbPass = process.env.DB_PASS;
 db(dbUrl, dbUser, dbPass, "ToDoApp");
 
 app.use(express.json());
+app.use(cors(corsOptions))
 app.use("/tarefas", require("./routes/tarefas.routes"));
 
 app.listen(process.env.PORT || port, () =>
